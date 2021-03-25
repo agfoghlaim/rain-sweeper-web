d7e8afe67ff85a1fd38e4f1bc7588126089e20cb
agfoghlaim <marieohara.gti@gmail.com>
2021-03-25 15:57:24 +0000

    Logo & animation.

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
 
 import classes from './layout.module.scss';
 import 'normalize.css';
@@ -7,11 +6,12 @@ import '../../styles/global.scss';
 import '../../styles/typography.scss';
 
 import Nav from '../nav/Nav';
-
+import Logo from '../logo/Logo';
 function Layout({ children }) {
   return (
     <div className={classes.layout}>
       <header>
+        <Logo />
         <Nav />
       </header>
       <>{children}</>
